f1d623591b1c30af5e6c999a2cd87347f68c37ce
Roland Hesse <rolandhesse11783@gmail.com>
2023-09-26 11:31:06 +0200

    set up database in MongoDB, connect to database, and adapt API-routes

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,17 +1,21 @@
 import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/connect.js";
+import Place from "@/db/models/Place.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
   if (!id) {
     return;
   }
 
-  const place = places.find((place) => place.id === id);
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
 
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
+    if (!place) {
+      return response.status(404).json({ status: "Not found" });
+    }
+    response.status(200).json(place);
   }
-
-  response.status(200).json(place);
 }
